b123a17a1532a8dc7467fe96af832fbe42c203d1
Jesus <jesusmaquera10@gmail.com>
2024-05-11 14:21:50 -0500

    auth settings

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use Throwable;
+
+class ApiController extends Controller
+{
+    public function register(Request $request){
+        try{
+            $validateUser= validator::make($request->all(),
+            [
+                'name'=>'required',
+                'email'=>'required|email|unique:users,email',
+                'password'=>'required',
+            ]);
+
+            if($validateUser->fails()){
+                return response()->json([
+                    'status'=>false,
+                    'message'=>'validation failed',
+                    'errors'=>$validateUser->errors(),
+                ],401);
+            }
+
+            $user = User::create([
+                'name'=>$request->name,
+                'email'=>$request->email,
+                'password'=>$request->password,
+            ]);
+
+            return response()->json([
+                'status'=>true,
+                'message'=>'User created successfully',
+                'token'=>$user->createToken('API TOKEN')->plainTextToken,
+            ],200);
+
+        }catch(Throwable $th){
+            return response()->json([
+                'status'=>false,
+                'message'=>$th->getMessage(),
+            ],500);
+        }
+    }
+
+    public function login(Request $request){
+        try{
+            
+            $validateUser= validator::make($request->all(),
+                [
+                    'password'=>'required',
+                    'email'=>'required|email',
+                ]);
+
+            if($validateUser->fails()){
+                return response()->json([
+                    'status'=>false,
+                    'message'=>'validation failed',
+                    'errors'=>$validateUser->errors(),
+                ],401);
+            }
+            if (!Auth::attempt($request->only(['email','password']))){
+                return response()->json([
+                    'status'=>false,
+                    'message'=>'Email & password does not match whit our record',
+                    
+                ],401);
+            }
+
+            $user = User::where('email',$request->email)->first();
+            return response()->json([
+                'status'=>true,
+                'message'=>'User logged in successfully',
+                'token'=>$user->createToken('API TOKEN')->plainTextToken,
+            ],200);
+
+        }catch(Throwable $th){
+            return response()->json([
+                'status'=>false,
+                'message'=>$th->getMessage(),
+            ],500);
+        }
+    }
+
+    public function profile(){
+        $userData = auth()->user();
+        return response()->json([
+            'status'=>true,
+            'message'=>'profile Information',
+            'data'=>$userData,
+            'id'=> auth()->user()->id
+        ],200);
+    }
+
+    public function logout(){
+        auth()->user()->tokens()->delete();
+        return response()->json([
+            'status'=>true,
+            'message'=>'user logged out',
+            'data'=>[],
+            
+        ],200);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\Api\ApiController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+//Register
+Route::post('/register', [ApiController::class,'register']);
+
+//Login
+Route::post('/login', [ApiController::class,'login']);
+
+Route::group([
+    'middleware'=>['auth:sanctum']
+],function(){
+    //profile
+    Route::get('/profile', [ApiController::class,'profile']);
+    
+    //logout
+    Route::get('/logout', [ApiController::class,'logout']);
+});
+
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
