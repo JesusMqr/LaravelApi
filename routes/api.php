c8ebc215b370bdc22952f7f2bfcca9703d9e2db2
Jesus <jesusmaquera10@gmail.com>
2024-05-11 20:10:05 -0500

    controlador de usuarios

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\Admin\UsersController;
 use App\Http\Controllers\Api\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -11,17 +12,19 @@ Route::post('/register', [ApiController::class,'register']);
 //Login
 Route::post('/login', [ApiController::class,'login']);
 
-Route::group([
-    'middleware'=>['auth:sanctum']
-],function(){
+Route::group(['middleware'=>['auth:sanctum']],function(){
     //profile
     Route::get('/profile', [ApiController::class,'profile']);
-    
     //logout
     Route::get('/logout', [ApiController::class,'logout']);
 });
 
+Route::group(['middleware'=>['auth:sanctum']],function(){
+    //users
+    Route::get('/users',[UsersController::class,'index']);
+});
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+
+Route::get('/user', function (Request $request) {
+   return $request->user();
+})->middleware('auth:sanctum');
